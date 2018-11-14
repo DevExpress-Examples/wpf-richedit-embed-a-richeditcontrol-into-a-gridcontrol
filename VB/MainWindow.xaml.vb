2e355d78c8a0bc9bbb7dadeb61818729c0247366
DevExpressExampleBot <fakeEmail>
2018-11-14 12:59:03 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,62 +1,71 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Collections.Generic
+﻿Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Documents
 Imports DevExpress.Xpf.RichEdit
 Imports DevExpress.XtraRichEdit
 
 Namespace RichInGridColumnWpf
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
+    Partial Public Class MainWindow
+        Inherits Window
 
-			Dim invoices As New List(Of Invoice) (New Invoice() {New Invoice With {.Id = 1, .Price = 10D, .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice1}\par}"}, New Invoice With {.Id = 2, .Price = 15D, .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice2}\par}"}, New Invoice With {.Id = 3, .Price = 20D, .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice3}\par}"}})
+        Public Sub New()
+            InitializeComponent()
 
-			gridControl1.DataSource = invoices
-		End Sub
+            Dim invoices As New List(Of Invoice) From { _
+                New Invoice With { _
+                    .Id = 1, _
+                    .Price = 10D, _
+                    .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice1}\par}" _
+                }, _
+                New Invoice With { _
+                    .Id = 2, _
+                    .Price = 15D, _
+                    .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice2}\par}" _
+                }, _
+                New Invoice With { _
+                    .Id = 3, _
+                    .Price = 20D, _
+                    .Description = "{\rtf1\deff0{\fonttbl{\f0 Times New Roman;}{\f1 Consolas;}}{\colortbl\red0\green0\blue0 ;\red0\green0\blue255 ;\red163\green21\blue21 ;}{\*\listoverridetable}{\stylesheet {\ql\cf0 Normal;}{\*\cs1\cf0 Default Paragraph Font;}{\*\cs2\sbasedon1\cf0 Line Number;}{\*\cs3\ul\cf1 Hyperlink;}}\sectd\pard\plain\ql{\f1\fs18\cf2 Invoice3}\par}" _
+                } _
+            }
 
-		Private Sub richEditControl_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim richEditControl As RichEditControl = CType(sender, RichEditControl)
+            gridControl1.ItemsSource = invoices
+        End Sub
+    End Class
 
-			richEditControl.ShowBorder = False
-			richEditControl.ActiveViewType = RichEditViewType.Simple
-			richEditControl.HorizontalScrollBarVisibility = System.Windows.Visibility.Collapsed
-			richEditControl.VerticalScrollBarVisibility = System.Windows.Visibility.Collapsed
+    Public Class Invoice
 
-		End Sub
-	End Class
+        Private id_Renamed As Integer
 
-	Public Class Invoice
-		Private id_Renamed As Integer
+        Public Property Id() As Integer
+            Get
+                Return id_Renamed
+            End Get
+            Set(ByVal value As Integer)
+                id_Renamed = value
+            End Set
+        End Property
 
-		Public Property Id() As Integer
-			Get
-				Return id_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				id_Renamed = value
-			End Set
-		End Property
-		Private price_Renamed As Decimal
+        Private price_Renamed As Decimal
 
-		Public Property Price() As Decimal
-			Get
-				Return price_Renamed
-			End Get
-			Set(ByVal value As Decimal)
-				price_Renamed = value
-			End Set
-		End Property
-		Private description_Renamed As String
+        Public Property Price() As Decimal
+            Get
+                Return price_Renamed
+            End Get
+            Set(ByVal value As Decimal)
+                price_Renamed = value
+            End Set
+        End Property
 
-		Public Property Description() As String
-			Get
-				Return description_Renamed
-			End Get
-			Set(ByVal value As String)
-				description_Renamed = value
-			End Set
-		End Property
-	End Class
+        Private description_Renamed As String
+
+        Public Property Description() As String
+            Get
+                Return description_Renamed
+            End Get
+            Set(ByVal value As String)
+                description_Renamed = value
+            End Set
+        End Property
+    End Class
 End Namespace
